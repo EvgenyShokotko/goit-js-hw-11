b8d55da397f9f38e8b77b8b017088c08f3171796
EvgenyShokotko <103266879+EvgenyShokotko@users.noreply.github.com>
2022-09-16 20:54:27 +0200

    notify aded

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 import { TEMPLATE } from './template-strings';
 import ImageApiService from './API-image-service';
 
@@ -13,36 +15,34 @@ function onSearchFormClick(event) {
   event.preventDefault();
   imageApiService.searchQuery = event.currentTarget.elements.searchQuery.value;
   if (imageApiService.searchQuery.trim() === '') {
-    return alert('Input somthing');
+    return Notiflix.Notify.info(
+      'The field can not be empty.Input something to start'
+    );
   }
 
   imageApiService.resetPage();
-  imageApiService
-    .fetchImages()
-    .then(loadedAll => {
-      if (loadedAll.total >= 1) {
-        alert(
-          `we found ${loadedAll.total}, but you can watch only ${loadedAll.totalHits}`
-        );
-      }
-      return loadedAll.hits;
-    })
-    .then(loadedImg => {
-      if (loadedImg.length === 0) {
-        return alert('We didnt find anything');
-      }
-      clearConteiner();
-      renderCardsHtml(loadedImg);
-    });
+  imageApiService.fetchImages().then(loadedAll => {
+    if (loadedAll.total >= 1) {
+      Notiflix.Notify.success(
+        `We found ${loadedAll.total} images, but you can watch free only ${loadedAll.totalHits}`
+      );
+    }
+    const loadedImg = loadedAll.hits;
+    if (loadedImg.length === 0) {
+      Notiflix.Notify.failure(
+        'OPPS, we didnt find anything. Try to input something else..'
+      );
+    }
+    clearConteiner();
+    renderCardsHtml(loadedImg);
+  });
 }
 
 function onBtnLoadMoreClick() {
-  imageApiService
-    .fetchImages()
-    .then(loadedAll => loadedAll.hits)
-    .then(loadedImg => {
-      renderCardsHtml(loadedImg);
-    });
+  imageApiService.fetchImages().then(loadedAll => {
+    const loadedImg = loadedAll.hits;
+    renderCardsHtml(loadedImg);
+  });
 }
 function renderCardsHtml(loadedImg) {
   gallery.insertAdjacentHTML('beforeend', TEMPLATE(loadedImg));
